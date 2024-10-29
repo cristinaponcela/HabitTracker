f763727b1a388fead53198f945b90c9dfa7eedd9
Cristina Poncela Cubeiro <cristina.poncela.cubeiro@gmail.com>
2024-10-29 14:14:50 +0100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Cristina Poncela on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
